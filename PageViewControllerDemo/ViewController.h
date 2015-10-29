2fc7993489b1d98999716a5613f3240566e426ff
beddup <beddup@163.com>
2015-10-29 16:49:57 +0800

    Initial Commit

---
+++ PageViewControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PageViewControllerDemo
+//
+//  Created by Amay on 10/29/15.
+//  Copyright © 2015 Beddup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PageViewControllerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PageViewControllerDemo
+//
+//  Created by Amay on 10/29/15.
+//  Copyright © 2015 Beddup. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
